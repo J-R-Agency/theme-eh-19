44cdc58e7c97b2539f1d014d743d2b544647703b
Greg Macoy <greg.macoy@jnragency.co.uk>
2019-12-18 15:44:25 +0000

    Setting up preview theme branch

---
+++ page-templates/flexible-content.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Template Name: Flexible Content Template
+ *
+ * @package understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+?>
+
+<div class="container-fluid no-margins">
+	
+	<div class="row">
+		<div class="col-12">
+			<?php if ( has_post_thumbnail() ): ?>
+				<?php $thumb = get_the_post_thumbnail_url(); ?>
+					<div class="hero" style="background: url('<?php echo $thumb; ?>') no-repeat; background-size: cover;"></div>	
+			<?php endif ?>
+		</div>
+	</div>
+	
+	<div class="content">
+		<div class="row">
+			<div class="col-12">
+				<h2>Flexible Content</h2>
+				<p>Example Para<p>
+
+
+<?php
+
+// Check value exists.
+if( have_rows('fc_content_block') ):
+
+    // Loop through rows.
+    while ( have_rows('fc_content_block') ) : the_row();
+
+        // Case: Paragraph layout.
+        if( get_row_layout() == 'module_content_block' ):
+            $mcb_title = get_sub_field('mcb_title');
+            // Do something...
+
+            echo "HERE IS SOME FLEXIBLE CONTENT: " . $mcb_title ;
+
+        // Case: Download layout.
+        elseif( get_row_layout() == 'download' ): 
+            $file = get_sub_field('file');
+            // Do something...
+
+        endif;
+
+    // End loop.
+    endwhile;
+
+// No value.
+else :
+    // Do something...
+endif;
+
+?>
+
+
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php 
+get_footer();
+?>
