c95bc89eb12d4174968fec14d5214b1a7eb4d235
Joy Ling <joy@Joys-MacBook-Pro.local>
2020-01-07 16:55:22 +0000

    Added additional stylizing to modules, focusing on Mentoring and Programme pages

--- loop-templates/content-flexible.php
+++ loop-templates/content-flexible.php
@@ -17,7 +17,10 @@ if( have_rows('fc_content_block') ):
     // Loop through rows.
     while ( have_rows('fc_content_block') ) : the_row();
 
-        // Case: Content Block.
+
+          // -------------------------- //
+         // --- CASE: CONTENT BLOCK ---//
+        // -------------------------- //
         if( get_row_layout() == 'module_content_block' ):
 
             $mcb_divider_icon = get_sub_field('mcb_divider_icon'); // Image
@@ -27,51 +30,131 @@ if( have_rows('fc_content_block') ):
             $mcb_cta_link = get_sub_field('mcb_cta_link'); // Link array
             $mcb_cta_style = get_sub_field('mcb_cta_style'); // Select
             $mcb_style = get_sub_field('mcb_style'); // Select
-
-            // Do something...
-						
-
-            echo "
-            <div class=\"module_content_block mt-5\"> " .
-	    
-	            	"<div class='row'>
-	            		<div class='col-12'>
-	            			<div class='line'>
-	            				<img src='". $mcb_divider_icon['url'] ."' alt='".$mcb_divider_icon['alt']." class='line-icon'>
-	            			</div>
-	            		</div>
-	            	</div>".
-	            	"<div class=\"mcb_title centered mt-3\"><div class='row'><h2>". $mcb_title . "</h2></div></div>" .
-	            	"<div class=\"mcb_content mt-3\"><div class='row'>". $mcb_content . "</div></div>";
-	            
-	        // Show CTA button if field is filled in. If not, don't display CTA.	
-        	if( !empty($mcb_cta_link) ):
-        		echo 
-        			"<a href=\"". $mcb_cta_link['url'] ."\">
-        				<div class=\"mcb_cta_link centered\">". $mcb_cta_text . "</div>
-        			</a>
-        		</div>";
-        	endif;
+			
+            echo 
+            "<!-- Module Content Block -->
+            <div class=\"module_content_block mt-5\">";
+            
+					// DIVIDER
+					if( !empty($mcb_divider_icon) ):
+		            	echo
+		            	"<div class='row'>
+		            		<div class='col-12'>
+		            			<div class='line'>
+		            				<img src='". $mcb_divider_icon['url'] ."' alt='".$mcb_divider_icon['alt']." class='line-icon'>
+		            			</div>
+		            		</div>
+		            	</div>";
+		            endif;
+		            
+		            
+		            // TITLE
+		            if( !empty($mcb_title) ):
+		            	if ($mcb_style == 'primary' or $mcb_style == 'secondary' or $mcb_style == 'tertiary'):
+		            		echo
+			            	"<div class=\"mcb_title centered mt-3\"><div class='row'><h2>". $mcb_title . "</h2></div></div>";
+			            elseif ($mcb_style == 'small'):
+			            	echo
+			            	"<div class=\"mcb_title centered mt-3\"><div class='row'><h5>". $mcb_title . "</h5></div></div>";
+			            endif;			            
+	            	endif;
+	            	
+	            	
+	            	
+	            	// PRIMARY STYLE
+	            	if ($mcb_style == 'primary'):
+	            		echo
+	            		"<div class=\"mcb_content mt-5 mb-5\">". $mcb_content;
+				        // Show CTA button if field is filled in. If not, don't display CTA.
+				        	
+			        	if( !empty($mcb_cta_link) ):
+			        		echo 
+			        			"<a href=\"". $mcb_cta_link['url'] ."\">
+			        				<div class=\"mcb_cta_link centered\">". $mcb_cta_text . "</div>
+			        			</a>";
+			        	endif;
+			        	echo "</div>"; // Close mcb_content       	            		
+					
+					
+					
+					// SECONDARY STYLE (TWO COLUMNS)
+	            	elseif ($mcb_style == 'secondary'):
+	            		echo
+	            		"<div class=\"mcb_content two-col mt-3\">". $mcb_content;
+	            		
+				        // Show CTA button if field is filled in. If not, don't display CTA.	
+			        	if( !empty($mcb_cta_link) ):
+			        		echo 
+			        			"<a href=\"". $mcb_cta_link['url'] ."\">
+			        				<div class=\"mcb_cta_link centered\">". $mcb_cta_text . "</div>
+			        			</a>";
+			        	endif;
+			        	echo "</div>"; // Close mcb_content	            			            		
+	            		
+	            		
+	            		
+	            	// TERTIARY STYLE (GREY BACKGROUND)
+	            	elseif ($mcb_style == 'tertiary' ):
+	            	
+	            		// PRIMARY CTA
+	            		if ($mcb_cta_style == 'primary') :
+		            		echo
+		            		"<div class=\"mcb_content grey-content-block mt-3\">". $mcb_content;
+	            		
+	            		// SECONDARY CTA
+	            		elseif ($mcb_cta_style == 'secondary') :
+		            		echo
+		            		"<div class=\"mcb_content grey-content-block green mt-3\">". $mcb_content;
+	            		
+	            		//TERTIARY CTA
+	            		elseif ($mcb_cta_style == 'tertiary') :
+		            		echo
+		            		"<div class=\"mcb_content grey-content-block navy mt-3\">". $mcb_content;
+	            		
+	            		endif;
+	            		
+				        // Show CTA button if field is filled in. If not, don't display CTA.	
+			        	if( !empty($mcb_cta_link)):
+			        		echo 
+			        			"<a href=\"". $mcb_cta_link['url'] ."\">
+			        				<div class=\"mcb_cta_link centered\">". $mcb_cta_text . "</div>
+			        			</a>";
+			        	endif;
+			        	
+			        echo "</div>"; // Close mcb_content
+			        	
+			        // SMALL
+	            	elseif ($mcb_style == 'small' ) :
+	            		echo
+	            		"<div class=\"mcb_content small mt-3\">". $mcb_content ."</div>";								        
+	            	endif;
         	
-				echo "</div>";
+			echo "</div>"; // Close module_content_block
 
-        // Case: Video Block.
+
+
+          // -------------------------- //
+         // ---- CASE: VIDEO BLOCK ----//
+        // -------------------------- //
         elseif( get_row_layout() == 'module_video_block' ): 
 
             $mvb_video_url = get_sub_field('mvb_video_url'); // oEmbed
 			$mvb_video_title = get_sub_field('mvb_video_title'); // Text
 			$mvb_style = get_sub_field('mvb_style'); // Select
-            
-            // Do something...
-            
+                        
             echo "
+            <!-- Module Video Block -->
             <div class=\"module_video_block\"><h2>Video Block: </h2>" .
             	"<div class=\"mvb_video_url\">". $mvb_video_url . "</div>" .
             	"<div class=\"mvb_video_title\">". $mvb_video_title . "</div>" .
             	"<div class=\"mvb_style\">". $mvb_style . "</div>" .
 			"</div>";
 
-        // Case: Module Icon Block.
+
+
+          // -------------------------- //
+         // - CASE: MODULE ICON BLOCK -//
+        // -------------------------- //
         elseif( get_row_layout() == 'module_icon_block' ): 
 
 			$mib_title = get_sub_field('mib_title'); // Text
@@ -79,10 +162,20 @@ if( have_rows('fc_content_block') ):
 			$mib_body = get_sub_field('mib_body'); // WYSIWYG block
 			$mib_style = get_sub_field('mib_style'); // Select
             
-            // Do something...
-
-            echo "
-            <div class=\"module_icon_block\">" .
+			echo "<!-- Module Icon Block -->";
+			
+			if ($mib_style == 'primary'):
+	            echo
+	            "<div class=\"module_icon_block mb-5\">";
+	        elseif ($mib_style == 'secondary'):
+	        	echo
+	            "<div class=\"module_icon_block green mb-5\">";
+	        elseif ($mib_style == 'tertiary'):
+	        	echo
+	            "<div class=\"module_icon_block navy mb-5\">";
+	        endif;   
+            
+            echo
             	"<div class=\"mib_title\"><h2>". $mib_title . "</h2></div>" .
             		"<div class=\"row\">" ;
 
@@ -111,21 +204,25 @@ if( have_rows('fc_content_block') ):
 				endif;
 			if( !empty($mib_body) ):	
 	            echo 
-	            	"</div>
-	            		<div class=\"mib_body mt-5\">". $mib_body . "</div>" .
-					"</div>";
+	            	"	<div class=\"mib_body mt-5\">". $mib_body . "</div>";
 			endif;
+			
+			echo "</div>
+			</div>"; // Close module_icon_block
+			
 
-        // Case: CTA Feature Block.
+		  // -------------------------- //
+         // - CASE: CTA FEATURE BLOCK -//
+        // -------------------------- //
+        
         elseif( get_row_layout() == 'module_cta_feature_block' ): 
 
             $mcfb_repeater = get_sub_field('mcfb_repeater'); // Repeater --- need sub fields loop
-            
-            // Do something...
-            
+                        
             echo "
+            <!-- CTA Feature Block -->
             <div class=\"module_cta_feature_block\">" .
-            	"<div class=\"row\">" ;
+            	"<div class=\"row centered\">" ;
 
 				// check if the repeater field has rows of data
 				if( have_rows('mcfb_repeater') ):
@@ -148,21 +245,30 @@ if( have_rows('fc_content_block') ):
 				            "<div class=\"mcfb_title mt-5 mb-3\"><h5>". $mcfb_title . "</h5></div>" .
 				            
 				            //Body
-				            	"<div class=\"mcfb_body mt-3\">". $mcfb_body . "</div>" .
-				            	
-				            // CTA Button
-				            	"<a href=\"". $mcfb_cta_link['url'] ."\">";
-				            	
-				            		if ($mcfb_cta_style == 'primary'):
-				            			echo
-											"<div class=\"mcb_cta_link centered\">". $mcfb_cta_text . "</div>";
+				            	"<div class=\"mcfb_body mt-3\">". $mcfb_body . "</div>";
+				            
+				            if( !empty($mcfb_cta_link) ):
+					            // CTA Button
+					            echo
+					            	"<a href=\"". $mcfb_cta_link['url'] ."\">";
+					            	
+					            		if ($mcfb_cta_style == 'primary'):
+					            			echo
+												"<div class=\"mcb_cta_link centered\">". $mcfb_cta_text . "</div>";
+											
+										elseif($mcfb_cta_style == 'secondary'):
+											echo
+												"<div class=\"mcb_cta_link navy centered\">". $mcfb_cta_text . "</div>";
+												
+										elseif($mcfb_cta_style == 'tertiary'):
+											echo
+												"<div class=\"mcb_cta_link green centered\">". $mcfb_cta_text . "</div>";
 										
-									elseif($mcfb_cta_style == 'secondary'):
-										echo
-											"<div class=\"mcb_cta_link secondary centered\">". $mcfb_cta_text . "</div>";
-									endif;
-							echo
-								"</a>" .					
+										endif;
+								echo
+									"</a>";
+							endif;
+							echo		
 				            "</div>";
 
 					endwhile;
@@ -173,10 +279,13 @@ if( have_rows('fc_content_block') ):
 
 				endif;
 
-           	echo "</div>";
+			echo "</div>
+			</div>"; // Close cta_feature_block
 
 
-        // Case:  Rich Callout Banner Block.
+          // ---------------------------------------- //
+         // ---- CASE: RICH CALLOUT BANNER BLOCK ----//
+        // ---------------------------------------- //
         elseif( get_row_layout() == 'module_rich_callout_banner_block' ): 
 
 			$mrcbb_title = get_sub_field('mrcbb_title'); // Text
@@ -185,18 +294,34 @@ if( have_rows('fc_content_block') ):
 			$mrcbb_cta_link = get_sub_field('mrcbb_cta_link'); // Link array
 			$mrcbb_cta_style = get_sub_field('mrcbb_cta_style'); // Select            
             
-            // Do something...
-            
-            echo "
-            <div class=\"module_rich_callout_banner_block\"><h2>Rich Callout Banner Block: </h2>" .
-            	"<div class=\"mrcbb_title\">". $mrcbb_title . "</div>" .
+			if ($mrcbb_cta_style == 'primary'):            
+	            echo "
+	            <div class=\"module_rich_callout_banner_block\">";
+	        elseif($mrcbb_cta_style == 'secondary'):
+	            echo "
+	            <div class=\"module_rich_callout_banner_block_navy\">";	        
+	        endif;
+	        
+	        echo
+            	"<div class=\"mrcbb_title mt-2 mb-2\">". $mrcbb_title . "</div>" .
             	"<div class=\"mrcbb_body\">". $mrcbb_body . "</div>" .
-            	"<div class=\"mrcbb_cta_text\">". $mrcbb_cta_text . "</div>" .
-            	"<div class=\"mrcbb_cta_link\">". $mrcbb_cta_link['url'] . $mrcbb_cta_link['title'] . $mrcbb_cta_link['target'] . "</div>" .
-            	"<div class=\"mrcbb_cta_style\">". $mrcbb_cta_style . "</div>" .
+            	"<div class=\"mrcbb_cta_text\">". $mrcbb_cta_text . "</div>";
+            	
+			    // Show CTA button if field is filled in. If not, don't display CTA.	
+	        	if( !empty($mrcbb_cta_link) ):
+	        		echo 
+	        			"<a href=\"". $mrcbb_cta_link['url'] ."\">
+	        				<div class=\"mcb_cta_link centered\">". $mrcbb_cta_text . "</div>
+	        			</a>";
+	        	endif;           	
+			echo
 			"</div>";
 
-        // Case:  Module Divider.
+
+
+          // ----------------------------- //
+         // ---- CASE: MODULE DIVIDER ----//
+        // ----------------------------- //
         elseif( get_row_layout() == 'module_divider' ): 
 
 			$md_icon = get_sub_field('md_icon'); // Image array
@@ -205,10 +330,14 @@ if( have_rows('fc_content_block') ):
             // Do something...
             
             echo "
-            <div class=\"module_divider\"><h2>Rich Callout Banner Block: </h2>" .
-            	"<div class=\"md_icon\">" . $md_icon['url'] . $md_icon['alt'] . "</div>" .
-            	"<div class=\"md_class\">" . $md_class . "</div>" .
-			"</div>";
+            <!-- Module Divider -->
+            <div class='row mt-5 mb-5'>
+        		<div class='col-12'>
+        			<div class='line'>
+        				<img src='". $md_icon['url'] ."' alt='".$md_icon['alt']." class='".$md_class."'>
+        			</div>
+        		</div>
+        	</div>";
 
         endif;
 
