c0111ebd7bc66a93a1b3867f5921f7d991594427
Joy Ling <joy@joys-mbp.home>
2020-01-08 13:01:07 +0000

    Stylised homepage and other changes

--- loop-templates/content-flexible.php
+++ loop-templates/content-flexible.php
@@ -238,12 +238,13 @@ if( have_rows('fc_content_block') ):
 						$mcfb_cta_style = get_sub_field('mcfb_cta_style'); // Select
 						
 						
-						echo "<div class=\"col-4 mt-5\">";
 						if ($mcfb_style == "primary"):
 							echo
+							"<div class=\"col-4 mt-5\">" .
 				            "<img src='". $mcfb_image['url'] ."' alt='".$mcfb_image['alt']."'>";
 				        elseif ($mcfb_style == "profile"):
 				        	echo
+				        	"<div class=\"col-fifth mt-5\">" .
 				        	"<img src='". $mcfb_image['url'] ."' alt='".$mcfb_image['alt']."' class='avatar-peach'>";
 			            endif;
 			            
