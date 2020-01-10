3d278227c28d2107b1d9fc0a6584295c954e4a99
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-01-10 18:47:22 +0000

    About Feature Image Block

--- page-templates/flexible-content--about.php
+++ page-templates/flexible-content--about.php
@@ -72,6 +72,63 @@ get_header();
 		
 	</div>
 	
+<?php
+
+
+	$afib_title = get_field('afib_title'); // Text
+	$afib_subtitle = get_field('afib_subtitle'); // Text 
+	$afib_image = get_field('afib_image'); // Image array
+	$afib_body = get_field('afib_body'); // Wysiwyg
+	$afib_twitter_cta = get_field('afib_twitter_cta'); // Link array
+	$afib_linkedin_cta = get_field('afib_linkedin_cta'); // Link array
+
+
+	echo "
+	    <!-- About Feature Image Block -->
+	    <div class=\"module_about_feature_image_block\">" .
+	    	"<div class=\"row centered\">" ;
+	if ($afib_title):
+		echo 
+        "<h2>" . $afib_title . "</h2>";
+    endif;
+	if ($afib_subtitle):
+		echo 
+        "<h3>" . $afib_subtitle . "</h3>";
+    endif;
+	if ($afib_image):
+		echo 
+        "<img src=\"". $afib_image['url'] ."\" alt=\"".$afib_image['alt']."\" class=\"avatar--about\">";
+    endif;
+	if ($afib_body):
+		echo 
+        "<div class=\"afib_body\">" . $afib_body . "</div>";
+    endif;
+	if ($afib_twitter_cta):
+		echo "
+		<div class=\"col-12\">
+			<a href=\"". $afib_twitter_cta['url'] ."\">
+		    	<div class=\"cta_link\">". $afib_twitter_cta['title'] . "</div>
+		    </a>
+		</div>";
+	endif;
+	if ($afib_linkedin_cta):
+		echo "
+		<div class=\"col-12\">
+			<a href=\"". $afib_linkedin_cta['url'] ."\">
+		    	<div class=\"cta_link\">". $afib_linkedin_cta['title'] . "</div>
+		    </a>
+		</div>";
+	endif;
+
+		echo		
+        "	</div>
+        </div>";
+
+
+?>
+
+
+
 	<!-- Primary Content CTA -->
 	<?php
 		if( have_rows('primary_content_cta') ):
