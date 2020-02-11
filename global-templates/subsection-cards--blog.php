1d6c763556ad028866b289557d884e48d2330260
Joy Ling <joy@Joys-MacBook-Pro.local>
2020-02-11 14:50:51 +0000

    Updated members area

--- global-templates/subsection-cards--blog.php
+++ global-templates/subsection-cards--blog.php
@@ -19,13 +19,13 @@ $wpb_query = new WP_Query(array(
 	'posts_per_page'=>4,
 	'orderby' => 1,
 )); ?>
-								
+
+<div class="row">							
 <?php if ( $wpb_query->have_posts() ) :
     $i = 0;
 	while ( $wpb_query->have_posts() ) : $wpb_query->the_post();
 		if ( $i == 0 ) :
 ?>
-		<div class="row">
 			<div class="col-12">
 				<a href="<?php the_permalink(); ?>" class="title">
 					<div class="blog-card">
@@ -41,16 +41,31 @@ $wpb_query = new WP_Query(array(
 					</div>
 				</a>
 			</div>
-		</div>
-		
+		<?php endif;?>
+		<?php if($i != 0 ): ?>
+					<div class="col-12 col-md-4">
+						<a href="<?php the_permalink(); ?>" class="title">
+							<div class="blog-card blog-card__small">
+								
+									<?php if ( has_post_thumbnail() ) {
+							    		echo "<img src=\"" . get_the_post_thumbnail_url() . "\">";
+									} else {
+										echo "<img src='".$themes_path."/theme-eh-19/images/blog/placeholder.jpg'>";
+									}?>
+								<h3><?php the_title(); ?></h3>
+								<p class="blog-excerpt"><?php the_excerpt() ?></p>
+							</div>
+						</a>
+					</div>
+		<?php endif;?>
 <?php $i++; ?>
-
 <?php
-        endif;
     endwhile;
 endif;
 ?>
+</div>
 
+<!--
 <div class="row">
 <?php if ( $wpb_query->have_posts() ) :
     $i = 0;	
@@ -78,5 +93,6 @@ endif;
 endif;
 ?>
 </div> 
+-->
 
 <?php wp_reset_postdata(); ?>
