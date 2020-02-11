3b5b19bfda8fbe16bca4e4f6d74862efa5c82a69
Joy Ling <joy@Joys-MacBook-Pro.local>
2020-02-11 15:03:50 +0000

    Updated members area

--- global-templates/subsection-cards--blog.php
+++ global-templates/subsection-cards--blog.php
@@ -14,10 +14,9 @@ $themes_path = get_theme_root_uri();
 <?php
 // QUERY ALL
 $wpb_query = new WP_Query(array(
-	'post_type'=>array('post'),
+	'post_type'=>'post',
 	'post_status'=>'publish',
 	'posts_per_page'=>4,
-	'orderby' => 1,
 )); ?>
 
 <div class="row">							
@@ -42,7 +41,22 @@ $wpb_query = new WP_Query(array(
 				</a>
 			</div>
 		<?php endif;?>
-
+		<?php if($i != 0 ): ?>
+					<div class="col-12 col-md-4">
+						<a href="<?php the_permalink(); ?>" class="title">
+							<div class="blog-card blog-card__small">
+								
+									<?php if ( has_post_thumbnail() ) {
+							    		echo "<img src=\"" . get_the_post_thumbnail_url() . "\">";
+									} else {
+										echo "<img src='".$themes_path."/theme-eh-19/images/blog/placeholder.jpg'>";
+									}?>
+								<h3><?php the_title(); ?></h3>
+								<p class="blog-excerpt"><?php the_excerpt() ?></p>
+							</div>
+						</a>
+					</div>
+		<?php endif;?>
 <?php $i++; ?>
 <?php
     endwhile;
